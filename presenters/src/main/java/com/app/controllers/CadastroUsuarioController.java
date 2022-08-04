506f115f0e7e52d655aad3c114003e3191866ee3
Guilherme Rodrigues <guilherme.rodrigues@spcbrasil.org.br>
2022-08-04 14:29:50 -0300

    permitir fazer cadastro de usuario

--- presenters/src/main/java/com/app/controllers/CadastroUsuarioController.java
+++ presenters/src/main/java/com/app/controllers/CadastroUsuarioController.java
@@ -1,7 +1,7 @@
 package com.app.controllers;
 
 import com.app.entites.Usuario;
-import com.app.request.UsuarioRequest;
+import com.app.request.CadastroRequest;
 import com.app.usecase.PersistenciaUsuario;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,15 +12,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/client")
+@RequestMapping("api/cadastro")
 @RequiredArgsConstructor
-public class UsuarioController {
+public class CadastroUsuarioController {
   private final PersistenciaUsuario persistenciaUsuario;
 
   @PostMapping
-  public ResponseEntity<Usuario> saveClient(@RequestBody UsuarioRequest usuarioRequest) {
-    final var usuario = new Usuario(usuarioRequest.getNome(),
-        usuarioRequest.getSenha(), usuarioRequest.isNaoExpirada(), usuarioRequest.isNaoBloqueada());
+  public ResponseEntity<Usuario> cadastroUsuario(@RequestBody CadastroRequest request) {
+    final var NAO_ESTA_EXPIRADO = true;
+    final var NAO_ESTA_BLOQUEADO = true;
+
+    final var usuario = new Usuario(
+        request.getNome(),
+        request.getSenha(),
+        NAO_ESTA_EXPIRADO,
+        NAO_ESTA_BLOQUEADO
+    );
+
     persistenciaUsuario.save(usuario);
     return new ResponseEntity<>(usuario, HttpStatus.OK);
   }
---
+++ presenters/src/main/java/com/app/request/CadastroRequest.java
@@ -0,0 +1,11 @@
+package com.app.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CadastroRequest {
+  private String nome;
+  private String senha;
+}
