94bc3251378a05b71b2e87df87acbccbc7d5a17b
guirodg <guilherme.rddev@gmail.com>
2024-02-15 20:55:55 -0300

    cors

--- infra/src/main/java/com/app/jwt/JWTConfiguracao.java
+++ infra/src/main/java/com/app/jwt/JWTConfiguracao.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -33,6 +34,7 @@ public class JWTConfiguracao extends WebSecurityConfigurerAdapter {
   }
 
   @Override
+  @CrossOrigin(origins = "*")
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable().authorizeRequests()
         .antMatchers(HttpMethod.POST, "/login").permitAll()
--- presenters/src/main/java/com/app/controllers/AtualizarPermissoesController.java
+++ presenters/src/main/java/com/app/controllers/AtualizarPermissoesController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class AtualizarPermissoesController {
 
   private final UsuarioUseCase usuarioUseCase;
